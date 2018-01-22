b57389430c53b14969746df9664afd74a29ca421
Dave Cross <dave@dave.org.uk>
2018-01-22 21:56:13 +0000

    Better handling of invalid dates.

--- Succession/lib/Succession.pm
+++ Succession/lib/Succession.pm
@@ -8,14 +8,20 @@ our $VERSION = '0.1';
 get qr{/(\d{4}-\d\d-\d\d)?$} => sub {
   my ($date) = splat;
 
+  my $date_err;
+
   unless ($date) {
     my $p = query_parameters;
     $date = $p->get('date');
   }
 
+  if (defined $date and $date !~ /^\d{4}-\d\d-\d\d$/) {
+    $date_err = 'Date must be in the format YYYY-MM-DD';
+    $date = undef;
+  }
+
   my $app = Succession::App->new($date // ());
 
-  my $date_err;
   if ($app->too_early) {
     $date_err = 'Date cannot be before ' .
       $app->earliest->strftime('%d %B %Y');
