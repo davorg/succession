84c8ddd765226ef30fa0ca25298ada6811649ea0
Dave Cross <dave@dave.org.uk>
2019-06-21 13:08:59 +0100

    Display children in correct order

--- Succession/lib/Succession/Schema/ResultSet/Person.pm
+++ Succession/lib/Succession/Schema/ResultSet/Person.pm
@@ -7,6 +7,12 @@ extends 'DBIx::Class::ResultSet';
 
 sub BUILDARGS { $_[2] }
 
+sub order_by_age {
+  my $self = shift;
+
+  return $self->search(undef, { order_by => 'born' });
+}
+
 sub find_by_slug {
   my $self = shift;
   my ($slug) = @_;
