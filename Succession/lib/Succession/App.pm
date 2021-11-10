601ab21156a4ddc00af2d040b4239770d28b8680
Dave Cross <dave@dave.org.uk>
2021-11-10 14:35:58 +0000

    Add some debugging

--- Succession/lib/Succession/App.pm
+++ Succession/lib/Succession/App.pm
@@ -159,7 +159,11 @@ has feed => (
 );
 
 sub _build_feed {
-  return XML::Feed->parse(URI->new('https://blog.lineofsuccession.co.uk/feed/'));
+  my $uri = URI->new('https://blog.lineofsuccession.co.uk/feed/');
+  warn "$uri\n";
+  my $feed = XML::Feed->parse($uri);
+  warn "$feed\n";
+  return $feed;
 }
 
 has title => (
