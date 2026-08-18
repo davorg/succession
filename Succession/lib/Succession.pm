b9573bd7232ab6c70f8d33fb4be8311d4a2fc3db
Dave Cross <dave@lineofsuccession.co.uk>
2026-08-18 09:36:29 +0100

    Change date for Adelaide

--- Succession/lib/Succession.pm
+++ Succession/lib/Succession.pm
@@ -15,7 +15,7 @@ use Succession::MCP;
 use Succession::Request;
 use Succession::RouteHelpers;
 
-our $VERSION = '0.11.2';
+our $VERSION = '0.11.3';
 
 hook before => sub {
   bless request, 'Succession::Request';
